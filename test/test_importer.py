0ccc46ac8817b4e799bc10ce19d40431cceec1f5
Rodrigo Navarro <rnavarro1@gmail.com>
2013-07-31 22:28:39 -0300

    Fixing importer test.

--- test/test_importer.py
+++ test/test_importer.py
@@ -3,13 +3,13 @@ from mongoengine import connect
 from enem.importer import GradeImporter
 from enem.models import City, School, State, Grade
 
+connect("geekie_test")
+
 class TestGradeImporter(TestCase):
     def setUp(self):
-        self.connection = connect("geekie_test")
-
-        self.state  = State(acronym="SP").save()
-        self.city   = City(name="Americana", state="SP", code=20).save()
-        self.school = School(name="Politec", state="SP", city_code=20, code=10).save()
+        (self.state, _)  = State.objects.get_or_create(acronym="SP")
+        (self.city, _)   = City.objects.get_or_create(name="Americana", state="SP", code=20)
+        (self.school, _) = School.objects.get_or_create(name="Politec", state="SP", city_code=20, code=10)
 
         result = dict(year=2011,
                       school=10,
@@ -28,28 +28,27 @@ class TestGradeImporter(TestCase):
         self.state.delete()
         self.city.delete()
         self.school.delete()
-        self.connection.close()
 
     def test_school_grade(self):
-        school = School.objects.get(code=35058836)
+        school = School.objects.get(code=10)
 
-        assertEqual(school.grades["2011"].nature_science[4], 1)
-        assertEqual(school.grades["2011"].human_science[4], 1)
-        assertEqual(school.grades["2011"].languages[4], 1)
-        assertEqual(school.grades["2011"].math[4], 1)
+        self.assertEqual(school.grades["2011"].nature_science[0], 1)
+        self.assertEqual(school.grades["2011"].human_science[3], 1)
+        self.assertEqual(school.grades["2011"].languages[8], 1)
+        self.assertEqual(school.grades["2011"].math[9], 1)
 
     def test_city_grade(self):
-        city = City.objects.get(code=3552403)
+        city = City.objects.get(code=20)
 
-        assertEqual(city.grades["2011"].nature_science[4], 1)
-        assertEqual(city.grades["2011"].human_science[4], 1)
-        assertEqual(city.grades["2011"].languages[4], 1)
-        assertEqual(city.grades["2011"].math[4], 1)
+        self.assertEqual(city.grades["2011"].nature_science[0], 1)
+        self.assertEqual(city.grades["2011"].human_science[3], 1)
+        self.assertEqual(city.grades["2011"].languages[8], 1)
+        self.assertEqual(city.grades["2011"].math[9], 1)
 
     def test_state_grade(self):
         state = State.objects.get(acronym="SP")
 
-        assertEqual(state.grades["2011"].nature_science[4], 1)
-        assertEqual(state.grades["2011"].human_science[4], 1)
-        assertEqual(state.grades["2011"].languages[4], 1)
-        assertEqual(state.grades["2011"].math[4], 1)
+        self.assertEqual(state.grades["2011"].nature_science[0], 1)
+        self.assertEqual(state.grades["2011"].human_science[3], 1)
+        self.assertEqual(state.grades["2011"].languages[8], 1)
+        self.assertEqual(state.grades["2011"].math[9], 1)
